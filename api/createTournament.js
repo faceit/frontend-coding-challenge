a33ed21784d1bd6d94543d9a13da010fb5544698
w01fgang <sumin@unix-center.ru>
2022-08-05 11:59:17 +0100

    chore: replace faker by @faker-js/faker
    Signed-off-by: w01fgang <sumin@unix-center.ru>

--- api/createTournament.js
+++ api/createTournament.js
@@ -1,4 +1,5 @@
-const faker = require('faker');
+// @ts-check
+const { faker } = require('@faker-js/faker');
 const random = require('lodash.random');
 const upperFirst = require('lodash.upperfirst');
 
@@ -7,19 +8,15 @@ const games = [
   'Dota 2',
   'Rocket League',
   'Battalion 1944',
-  'League of Legends'
+  'League of Legends',
 ];
 
 function createTournament(name) {
   return {
-    id: faker.random.uuid(),
+    id: faker.datatype.uuid(),
     name:
       name ||
-      faker.lorem
-        .words(random(2, 4))
-        .split(' ')
-        .map(upperFirst)
-        .join(' '),
+      faker.lorem.words(random(2, 4)).split(' ').map(upperFirst).join(' '),
     organizer: faker.lorem
       .words(random(1, 2))
       .split(' ')
@@ -28,9 +25,9 @@ function createTournament(name) {
     game: games[random(0, games.length - 1)],
     participants: {
       current: random(0, 256),
-      max: 256
+      max: 256,
     },
-    startDate: new Date().toISOString()
+    startDate: new Date().toISOString(),
   };
 }
 
