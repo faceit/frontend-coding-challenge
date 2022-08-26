bd62c005303c83edea60a5d7e390aff091493ae5
w01fgang <sumin@unix-center.ru>
2022-08-26 07:09:40 +0100

    feat: add breakpoints to theme
    Signed-off-by: w01fgang <sumin@unix-center.ru>

--- src/theme.ts
+++ src/theme.ts
@@ -3,65 +3,72 @@ const theme = {
     primary: {
       main: '#FF5500',
       light: '#FD6E35',
-      dark: '#CC4400'
+      dark: '#CC4400',
     },
     secondary: {
       main: '#FF5500',
       light: '#FD6E35',
-      dark: '#CC4400'
+      dark: '#CC4400',
     },
     attention: {
       main: '#D64242',
       light: '#F04A4A',
-      dark: 'BD3A3A'
+      dark: 'BD3A3A',
     },
     background: {
       body: '#161616',
       base: '#1F1F1F',
       alt1: '#303030',
-      alt2: '#484848'
+      alt2: '#484848',
     },
     text: {
-      primary: '#FFF'
-    }
+      primary: '#FFF',
+    },
   },
   spacing: (multiplier = 1) => `${4 * multiplier}px`,
   borderRadius: '4px',
   typography: {
     h1: {
       'font-weight': 'bold',
-      'font-size': '68px'
+      'font-size': '68px',
     },
     h2: {
       'font-weight': 'bold',
-      'font-size': '50px'
+      'font-size': '50px',
     },
     h3: {
       'font-weight': 'bold',
-      'font-size': '38px'
+      'font-size': '38px',
     },
     h4: {
       'font-weight': 'bold',
-      'font-size': '28px'
+      'font-size': '28px',
     },
     h5: {
       'font-weight': 'bold',
-      'font-size': '22px'
+      'font-size': '22px',
     },
     h6: {
       'font-weight': 'bold',
-      'font-size': '16px'
+      'font-size': '16px',
     },
     body: {
       'font-weight': 'normal',
-      'font-size': '14px'
+      'font-size': '14px',
     },
     button: {
       'font-weight': 'bold',
       'font-size': '14px',
-      'text-transform': 'uppercase'
-    }
-  }
+      'text-transform': 'uppercase',
+    },
+  },
+  breakpoints: {
+    s: '480px',
+    m: '768px',
+    l: '992px',
+    xl: '1200px',
+    xxl: '1620px',
+  },
 };
 
 export default theme;
